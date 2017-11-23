82191632e706f3aebe954d9e4fcce46313736950
MisterTakaashi <soapmctravich@gmail.com>
2017-11-23 20:38:25 +0100

    Definition port dans variable

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.static(__dirname + '/public'));
 router.init(app);
 
 // On demande à l'application d'écouter soit sur le port défini par le système (Heroku définie le 80 de base), soit sur le port 8080
-app.listen(process.env.PORT || 8080, () => {
-    console.log("Server listening on port 8080");
+let port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
 });
